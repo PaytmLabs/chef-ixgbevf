84625ff37c95762c527d4c1a8a5bdd9e61d37f1e
Joe Hohertz <jhohertz@gmail.com>
2016-01-08 13:23:13 -0500

    bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jhohertz@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures ixgbevf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'yum-epel', '>= 0.0.0'
 depends          'apt', '>= 0.0.0'
